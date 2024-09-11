1aa246abe018969aa1dcf1dea9cabe99cfdf243b
Timothy Hatcher <timothy@apple.com>
2024-09-11 13:55:58 -0700

    Cherry-pick 6cb5017d237e. rdar://135809898
        "Exceeded storage quota" error even with unlimitedStorage permission.
        https://webkit.org/b/279385
        rdar://131762857
    
        Reviewed by Brian Weinstein.
    
        The `webExtensionUnlimitedStorageQuotaBytes` global was incorrectly defined as a `double`, while
        `quotaForStorageType()` returns a `size_t`. This mismatch caused type truncation issues, leading
        to the quota check failing.
    
        Additionally, a crash was occurring when deleting the last storage value. The issue was due to
        finalizing an SQLite statement asynchronously after the database had been closed and deleted.
    
        * Source/WebKit/Shared/Extensions/WebExtensionConstants.h:
        * Source/WebKit/Shared/Extensions/_WKWebExtensionSQLiteStatement.mm:
        (-[_WKWebExtensionSQLiteStatement dealloc]): Make sure the database is not closed.
        * Source/WebKit/UIProcess/Extensions/Cocoa/API/WebExtensionContextAPIStorageCocoa.mm:
        (WebKit::WebExtensionContext::storageSet): Fix typo for quotaForStorageType.
        * Source/WebKit/UIProcess/Extensions/Cocoa/WebExtensionContextCocoa.mm:
        (WebKit::WebExtensionContext::quotaForStorageType): Renamed to fix typo.
        (WebKit::WebExtensionContext::quoataForStorageType): Deleted.
        * Source/WebKit/UIProcess/Extensions/WebExtensionContext.h:
    
        Canonical link: https://commits.webkit.org/283383@main
    
    Canonical link: https://commits.webkit.org/283286.11@safari-7620-branch

--- Source/WebKit/UIProcess/Extensions/Cocoa/WebExtensionContextCocoa.mm
+++ Source/WebKit/UIProcess/Extensions/Cocoa/WebExtensionContextCocoa.mm
@@ -4673,7 +4673,7 @@ void WebExtensionContext::setSessionStorageAllowedInContentScripts(bool allowed)
     extensionController()->sendToAllProcesses(Messages::WebExtensionContextProxy::SetStorageAccessLevel(allowed), identifier());
 }
 
-size_t WebExtensionContext::quoataForStorageType(WebExtensionDataType storageType)
+size_t WebExtensionContext::quotaForStorageType(WebExtensionDataType storageType)
 {
     switch (storageType) {
     case WebExtensionDataType::Local:
