ebd4bfe3b15cb9d6df10b1442941be62eaad78cf
Chris Dumez <cdumez@apple.com>
2024-10-16 13:26:36 -0700

    Cherry-pick c78fd3d92bca. rdar://138036710
        Crash under ValidationBubble::showRelativeTo() when the view doesn't have a window
        https://bugs.webkit.org/show_bug.cgi?id=281551
        rdar://137514579
    
        Reviewed by Wenson Hsieh.
    
        Passing a view that isn't part of a window to `[NSPopover showRelativeTo:]` crashes.
        However, this can happen if the user quickly switches to another tab as form validation
        is happening. We now early return if the view no longer has a window when
        ValidationBubble::showRelativeTo() gets called.
    
        * Source/WebCore/platform/mac/ValidationBubbleMac.mm:
        (WebCore::ValidationBubble::showRelativeTo):
        * Tools/TestWebKitAPI/Tests/WebKitCocoa/FormValidation.mm:
        (TEST(FormValidation, PresentingFormValidationUIWithoutViewControllerDoesNotCrash)):
        (TEST(FormValidation, FormValidationOnUnparentedWindowDoesNotCrash)):
        * Tools/TestWebKitAPI/cocoa/TestWKWebView.h:
        * Tools/TestWebKitAPI/cocoa/TestWKWebView.mm:
        (-[TestWKWebView removeFromTestWindow]):
    
        Canonical link: https://commits.webkit.org/285264@main
    
    Canonical link: https://commits.webkit.org/283286.294@safari-7620-branch

--- Tools/TestWebKitAPI/cocoa/TestWKWebView.mm
+++ Tools/TestWebKitAPI/cocoa/TestWKWebView.mm
@@ -918,6 +918,12 @@ static InputSessionChangeCount nextInputSessionChangeCount()
 #endif
 }
 
+- (void)removeFromTestWindow
+{
+    if (_hostWindow)
+        [self removeFromSuperview];
+}
+
 - (void)clearMessageHandlers:(NSArray *)messageNames
 {
     for (NSString *messageName in messageNames)
