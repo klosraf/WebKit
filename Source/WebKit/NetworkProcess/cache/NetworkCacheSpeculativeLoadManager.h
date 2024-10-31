42b3020af4da5314b7e46ffe7be880579830f6b5
Chris Dumez <cdumez@apple.com>
2024-10-31 09:33:41 -0700

    Cherry-pick 33be0a4c0f11. rdar://138894988
        Crash under NetworkCache::SpeculativeLoadManager::~SpeculativeLoadManager()
        https://bugs.webkit.org/show_bug.cgi?id=282297
        rdar://138800566
    
        Reviewed by Ryosuke Niwa.
    
        Made several fixes / hardening to hopefully resolve the crash:
        1. The LowPowerModeNotifier destructor now makes sure to unregister the
           notification handler from NSNotificationCenter in addition to clear
           the notifier pointer. This guards against potential races.
        2. Made WebLowPowerModeObserver.notifier a CheckedPtr instead of a raw
           pointer for extra safety.
        3. In the NetworkCache constructor, capture `weakThis` instead of `this`
           in the lambda we pass to the LowPowerModeNotifier. We also convert
           `weakThis` to a `protectedThis` inside the lambda to guarantee
           lifetime.
        4. Update SpeculativeLoadManager::m_storage to be a ThreadSafeWeakPtr
           instead of a CheckedRef since m_storage is already ThreadSafeRefCounted.
        5. Add some threading assertions to help catch potential multi-threading
           issues.
    
        * Source/WebCore/platform/LowPowerModeNotifier.h:
        * Source/WebCore/platform/cocoa/LowPowerModeNotifier.mm:
        (-[WebLowPowerModeObserver dealloc]):
        (-[WebLowPowerModeObserver detach]):
        (-[WebLowPowerModeObserver _didReceiveLowPowerModeChange]):
        (WebCore::LowPowerModeNotifier::LowPowerModeNotifier):
        (WebCore::LowPowerModeNotifier::~LowPowerModeNotifier):
        * Source/WebKit/NetworkProcess/cache/NetworkCache.cpp:
        (WebKit::NetworkCache::Cache::Cache):
        * Source/WebKit/NetworkProcess/cache/NetworkCache.h:
        (WebKit::NetworkCache::Cache::protectedStorage const):
        * Source/WebKit/NetworkProcess/cache/NetworkCacheSpeculativeLoadManager.cpp:
        (WebKit::NetworkCache::SpeculativeLoadManager::protectedStorage const):
        (WebKit::NetworkCache::SpeculativeLoadManager::retrieveSubresourcesEntry):
        (WebKit::NetworkCache::SpeculativeLoadManager::~SpeculativeLoadManager): Deleted.
        * Source/WebKit/NetworkProcess/cache/NetworkCacheSpeculativeLoadManager.h:
        * Source/WebKit/NetworkProcess/cache/NetworkCacheStorage.h:
    
        Canonical link: https://commits.webkit.org/285883@main
    
    Canonical link: https://commits.webkit.org/283286.395@safari-7620-branch

--- Source/WebKit/NetworkProcess/cache/NetworkCacheSpeculativeLoadManager.h
+++ Source/WebKit/NetworkProcess/cache/NetworkCacheSpeculativeLoadManager.h
@@ -93,7 +93,7 @@ private:
     Ref<Storage> protectedStorage() const;
 
     WeakRef<Cache> m_cache;
-    CheckedRef<Storage> m_storage;
+    ThreadSafeWeakPtr<Storage> m_storage; // Not expected to be null.
 
     class PendingFrameLoad;
     HashMap<GlobalFrameID, RefPtr<PendingFrameLoad>> m_pendingFrameLoads;
