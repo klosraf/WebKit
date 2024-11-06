72ec467afa6726ccaca4307c040357fc649ef474
Dan Robson <dtr_bugzilla@apple.com>
2024-11-06 11:51:55 -0800

    Revert af8397ad9f69. rdar://139236248
    This reverts commit 7430043d67daf105186ff14ecb9320b14767d6e9.
    
    Canonical link: https://commits.webkit.org/283286.440@safari-7620.1.16-branch

--- Source/WebKit/UIProcess/API/Cocoa/WKPreferences.mm
+++ Source/WebKit/UIProcess/API/Cocoa/WKPreferences.mm
@@ -1667,24 +1667,6 @@ static WebCore::EditableLinkBehavior toEditableLinkBehavior(_WKEditableLinkBehav
     return _preferences->videoFullscreenRequiresElementFullscreen();
 }
 
-- (void)_setSpatialVideoEnabled:(BOOL)enabled
-{
-#if ENABLE(LINEAR_MEDIA_PLAYER)
-    _preferences->setSpatialVideoEnabled(enabled);
-#else
-    UNUSED_PARAM(enabled);
-#endif
-}
-
-- (BOOL)_spatialVideoEnabled
-{
-#if ENABLE(LINEAR_MEDIA_PLAYER)
-    return _preferences->spatialVideoEnabled();
-#else
-    return NO;
-#endif
-}
-
 @end
 
 @implementation WKPreferences (WKDeprecated)
