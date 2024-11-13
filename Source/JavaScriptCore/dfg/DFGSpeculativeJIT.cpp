aeb6a25452317038cc4ca02b4b2c6f946e9ff3ba
Daniel Liu <danlliu@umich.edu>
2024-11-13 12:14:45 -0800

    Cherry-pick ded4d02c0a93. rdar://139747120
        Don't allocate DFG register after a slow path
        https://bugs.webkit.org/show_bug.cgi?id=283063
        rdar://139747120
    
        Reviewed by Yusuke Suzuki.
    
        Allocating a DFG register after a slow path means that if the slow path
        is taken, we end up with an incorrect global state.
    
        * Source/JavaScriptCore/dfg/DFGSpeculativeJIT.cpp:
        (JSC::DFG::SpeculativeJIT::compilePutByValForIntTypedArray):
    
        Canonical link: https://commits.webkit.org/283286.475@safari-7620-branch

--- Source/JavaScriptCore/dfg/DFGSpeculativeJIT.cpp
+++ Source/JavaScriptCore/dfg/DFGSpeculativeJIT.cpp
@@ -4222,6 +4222,14 @@ void SpeculativeJIT::compilePutByValForIntTypedArray(Node* node, TypedArrayType 
         }
     }
 
+    GPRReg scratch2GPR = InvalidGPRReg;
+#if USE(JSVALUE64)
+    if (node->arrayMode().mayBeResizableOrGrowableSharedTypedArray()) {
+        scratch2.emplace(this);
+        scratch2GPR = scratch2->gpr();
+    }
+#endif
+
     bool result = getIntTypedArrayStoreOperand(
         value, propertyReg,
 #if USE(JSVALUE32_64)
@@ -4232,14 +4240,6 @@ void SpeculativeJIT::compilePutByValForIntTypedArray(Node* node, TypedArrayType 
         noResult(node);
         return;
     }
-
-    GPRReg scratch2GPR = InvalidGPRReg;
-#if USE(JSVALUE64)
-    if (node->arrayMode().mayBeResizableOrGrowableSharedTypedArray()) {
-        scratch2.emplace(this);
-        scratch2GPR = scratch2->gpr();
-    }
-#endif
 
     GPRReg valueGPR = value.gpr();
     GPRReg scratchGPR = scratch.gpr();
