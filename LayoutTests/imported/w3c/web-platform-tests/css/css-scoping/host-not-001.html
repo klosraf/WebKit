klosraf/WebKit

commit b2e941c46d19000ece893d2f79e9ff52fda755a5

Fix :is(:host) both as a subject and as an ancestor https://bugs.webkit.org/show_bug.cgi?id=264628 rdar://118582384
Reviewed by Antti Koivisto.

As ancestor: we always follow the Shadow DOM host, and check a posteriori
that we have successfully matched an :host pseudoclass.

As subject: rules containg compound :host will not end up in the host bucket
but in the universal bucket of the shadow host, we mark those as such and collect them when needed.

* LayoutTests/TestExpectations:
* LayoutTests/fast/css/host-matching-expected.html: Added.
* LayoutTests/fast/css/host-matching.html: Added.
* Source/WebCore/css/CSSSelector.cpp:
(WebCore::CSSSelector::RareData::deepCopy const):
(WebCore::CSSSelector::isHostPseudoClass const):
* Source/WebCore/css/CSSSelector.h:
* Source/WebCore/css/SelectorChecker.cpp:
(WebCore::SelectorChecker::matchHostPseudoClass const):
(WebCore::localContextForParent):
(WebCore::SelectorChecker::matchRecursively const):
(WebCore::SelectorChecker::checkOne const):
* Source/WebCore/css/SelectorChecker.h:
* Source/WebCore/css/parser/CSSSelectorParser.cpp:
(WebCore::CSSSelectorParser::prependTypeSelectorIfNeeded):
* Source/WebCore/css/parser/MutableCSSSelector.cpp:
(WebCore::MutableCSSSelector::isHostPseudoSelector const): Deleted.
* Source/WebCore/css/parser/MutableCSSSelector.h:
(WebCore::MutableCSSSelector::isHostPseudoClass const):
* Source/WebCore/style/ElementRuleCollector.cpp:
(WebCore::Style::ElementRuleCollector::matchHostPseudoClassRules):
* Source/WebCore/style/RuleSet.cpp:
(WebCore::Style::hasHostPseudoClassSubjectInSelectorList):
(WebCore::Style::isHostSelectorMatchingInShadowTree):
(WebCore::Style::RuleSet::addRule):
* Source/WebCore/style/RuleSet.h:
(WebCore::Style::RuleSet::hasHostPseudoClassRulesInUniversalBucket const):

Canonical link: https://commits.webkit.org/281963@main

Source/WebCore/css/SelectorChecker.cpp
@@ -83,6 +83,7 @@ struct SelectorChecker::LocalContext {
     bool hasSelectionPseudo { false };
     bool hasViewTransitionPseudo { false };
     bool mustMatchHostPseudoClass { false };
+    bool matchedHostPseudoClass { false };
 };
 
 static inline void addStyleRelation(SelectorChecker::CheckingContext& checkingContext, const Element& element, Style::Relation::Type type, unsigned value = 1)
@@ -213,6 +214,7 @@ bool SelectorChecker::matchHostPseudoClass(const CSSSelector& selector, const El
         return false;
 
     if (auto* selectorList = selector.selectorList()) {
+        ASSERT(selectorList->listSize() == 1);
         LocalContext context(*selectorList->first(), element, VisitedMatchType::Enabled, std::nullopt);
         context.inFunctionalPseudoClass = true;
         context.pseudoElementEffective = false;
@@ -268,13 +270,11 @@ static SelectorChecker::LocalContext localContextForParent(const SelectorChecker
         return updatedContext;
     }
 
-    // Move to the shadow host if matching :host and the parent is the shadow root.
-    if (context.selector->match() == CSSSelector::Match::PseudoClass && context.selector->pseudoClass() == CSSSelector::PseudoClass::Host) {
-        if (auto* shadowRoot = dynamicDowncast<ShadowRoot>(context.element->parentNode())) {
-            updatedContext.element = shadowRoot->host();
-            updatedContext.mustMatchHostPseudoClass = true;
-            return updatedContext;
-        }
+    // Move to the shadow host if the parent is the shadow root and mark that we must match :host
+    if (auto* shadowRoot = dynamicDowncast<ShadowRoot>(context.element->parentNode())) {
+        updatedContext.element = shadowRoot->host();
+        updatedContext.mustMatchHostPseudoClass = true;
+        return updatedContext;
     }
 
     updatedContext.element = context.element->parentElement();
@@ -287,7 +287,7 @@ static SelectorChecker::LocalContext localContextForParent(const SelectorChecker
 // * SelectorFailsLocally     - the selector fails for the element e
 // * SelectorFailsAllSiblings - the selector fails for e and any sibling of e
 // * SelectorFailsCompletely  - the selector fails for e and any sibling or ancestor of e
-SelectorChecker::MatchResult SelectorChecker::matchRecursively(CheckingContext& checkingContext, const LocalContext& context, PseudoIdSet& dynamicPseudoIdSet) const
+SelectorChecker::MatchResult SelectorChecker::matchRecursively(CheckingContext& checkingContext, LocalContext& context, PseudoIdSet& dynamicPseudoIdSet) const
 {
     MatchType matchType = MatchType::Element;
 
@@ -336,8 +336,12 @@ SelectorChecker::MatchResult SelectorChecker::matchRecursively(CheckingContext& 
 
     // Prepare next selector
     const CSSSelector* leftSelector = context.selector->tagHistory();
-    if (!leftSelector)
+    if (!leftSelector) {
+        if (context.mustMatchHostPseudoClass && !context.matchedHostPseudoClass)
+            return MatchResult::fails(Match::SelectorFailsCompletely);
+
         return MatchResult::matches(matchType);
+    }
 
     LocalContext nextContext(context);
     nextContext.selector = leftSelector;
@@ -695,7 +699,7 @@ static inline bool tagMatches(const Element& element, const CSSSelector& simpleS
     return namespaceURI == starAtom() || namespaceURI == element.namespaceURI();
 }
 
-bool SelectorChecker::checkOne(CheckingContext& checkingContext, const LocalContext& context, MatchType& matchType) const
+bool SelectorChecker::checkOne(CheckingContext& checkingContext, LocalContext& context, MatchType& matchType) const
 {
     const Element& element = *context.element;
     const CSSSelector& selector = *context.selector;
@@ -704,7 +708,8 @@ bool SelectorChecker::checkOne(CheckingContext& checkingContext, const LocalCont
         // :host doesn't combine with anything except pseudo elements.
         bool isHostPseudoClass = selector.match() == CSSSelector::Match::PseudoClass && selector.pseudoClass() == CSSSelector::PseudoClass::Host;
         bool isPseudoElement = selector.match() == CSSSelector::Match::PseudoElement;
-        if (!isHostPseudoClass && !isPseudoElement)
+        // We can early return when we know it's neither :host, a compound like :is(:host), a pseudo-element.
+        if (!isHostPseudoClass && !isPseudoElement && !selector.selectorList())
             return false;
     }
 
@@ -881,15 +886,16 @@ bool SelectorChecker::checkOne(CheckingContext& checkingContext, const LocalCont
                 bool hasMatchedAnything = false;
 
                 MatchType localMatchType = MatchType::VirtualPseudoElementOnly;
-                for (const CSSSelector* subselector = selector.selectorList()->first(); subselector; subselector = CSSSelectorList::next(subselector)) {
+                for (const auto& subselector : *selector.selectorList()) {
                     LocalContext subcontext(context);
                     subcontext.inFunctionalPseudoClass = true;
                     subcontext.pseudoElementEffective = context.pseudoElementEffective;
-                    subcontext.selector = subselector;
-                    subcontext.firstSelectorOfTheFragment = subselector;
+                    subcontext.selector = &subselector;
+                    subcontext.firstSelectorOfTheFragment = &subselector;
                     subcontext.pseudoElementIdentifier = std::nullopt;
                     PseudoIdSet localDynamicPseudoIdSet;
                     MatchResult result = matchRecursively(checkingContext, subcontext, localDynamicPseudoIdSet);
+                    context.matchedHostPseudoClass |= subcontext.matchedHostPseudoClass;
 
                     // Pseudo elements are not valid inside :is()/:matches()
                     if (localDynamicPseudoIdSet)
@@ -1118,7 +1124,10 @@ bool SelectorChecker::checkOne(CheckingContext& checkingContext, const LocalCont
         case CSSSelector::PseudoClass::Host: {
             if (!context.mustMatchHostPseudoClass)
                 return false;
-            return matchHostPseudoClass(selector, element, checkingContext);
+            if (!matchHostPseudoClass(selector, element, checkingContext))
+                return false;
+            context.matchedHostPseudoClass = true;
+            return true;
         }
         case CSSSelector::PseudoClass::Defined:
             return isDefinedElement(element);

LayoutTests/TestExpectations
@@ -7416,14 +7416,6 @@ imported/w3c/web-platform-tests/scroll-animations/view-timelines/range-boundary.
 # webkit.org/b/229520 CSS @scope unimplemented
 imported/w3c/web-platform-tests/css/css-cascade/scope-visited.html [ ImageOnlyFailure ]
 
-# :is(:host) https://bugs.webkit.org/show_bug.cgi?id=264628
-imported/w3c/web-platform-tests/css/css-nesting/host-nesting-001.html [ ImageOnlyFailure ]
-imported/w3c/web-platform-tests/css/css-nesting/host-nesting-002.html [ ImageOnlyFailure ]
-imported/w3c/web-platform-tests/css/css-nesting/host-nesting-005.html [ ImageOnlyFailure ]
-imported/w3c/web-platform-tests/css/css-scoping/host-is-001.html [ ImageOnlyFailure ]
-imported/w3c/web-platform-tests/css/css-scoping/host-is-002.html [ ImageOnlyFailure ]
-imported/w3c/web-platform-tests/css/css-scoping/host-is-005.html [ ImageOnlyFailure ]
-
 webkit.org/b/263923 animations/transition-and-animation-2.html [ Failure Pass ]
 
 # CloseWatcher related timeouts

LayoutTests/fast/css/host-matching-expected.html
@@ -1,0 +1,17 @@
+<!doctype html>
+<style>
+.test {
+  background-color: green;
+  width: 30px;
+  height: 30px;
+  margin: 10px;
+}
+</style>
+<div id="host1" class="test"> </div>
+<div id="host2" class="test"> </div>
+<div id="host3" class="test"> </div>
+<div id="host4" class="test"> </div>
+<div id="host5" class="test"> </div>
+<div id="host6" class="test"> </div>
+<div id="host5" class="test"> </div>
+<div id="host6" class="test"> </div>

LayoutTests/fast/css/host-matching.html
@@ -1,0 +1,148 @@
+<!doctype html>
+<div id="host1"> </div>
+<div id="host2"> </div>
+<div id="host3"> </div>
+<div id="host4"> </div>
+<div id="host5"> </div>
+<div id="host6"> </div>
+<div id="host7"> </div>
+<div><div id="host8"> </div></div>
+<script>
+  host1.attachShadow({mode: "open"}).innerHTML = `
+    <style>
+      .nested {
+        background-color: red;
+        width: 30px;
+        height: 30px;
+        margin: 10px;
+      }
+      :is(:host) .nested {
+        background-color: green;
+        width: 30px;
+        height: 30px;
+        margin: 10px;
+      }
+    </style>
+    <div class="nested"></div>
+  `;
+
+  host2.attachShadow({mode: "open"}).innerHTML = `
+    <style>
+      .nested {
+        background-color: red;
+        width: 30px;
+        height: 30px;
+        margin: 10px;
+      }
+      :is(foo,:host) .nested {
+        background-color: green;
+        width: 30px;
+        height: 30px;
+        margin: 10px;
+      }
+    </style>
+    <div class="nested"></div>
+  `;
+
+  host3.attachShadow({mode: "open"}).innerHTML = `
+    <style>
+      .nested {
+        background-color: green;
+        width: 30px;
+        height: 30px;
+        margin: 10px;
+      }
+      :not(:host) .nested {
+        background-color: red;
+      }
+    </style>
+    <div class="nested"></div>
+  `;
+
+  host4.attachShadow({mode: "open"}).innerHTML = `
+    <style>
+      .nested {
+        background-color: red;
+        width: 30px;
+        height: 30px;
+        margin: 10px;
+      }
+      :is(:is(:host)) .nested {
+        background-color: green;
+        width: 30px;
+        height: 30px;
+        margin: 10px;
+      }
+    </style>
+    <div class="nested"></div>
+  `;
+
+  host5.attachShadow({mode: "open"}).innerHTML = `
+    <style>
+      div {
+        background-color: red;
+        width: 30px;
+        height: 30px;
+        margin: 10px;
+      }
+      :is(:host(div)) .nested {
+        background-color: green;
+        width: 30px;
+        height: 30px;
+        margin: 10px;
+      }
+    </style>
+    <div class="nested"></div>
+  `;
+
+  host6.innerHTML = `
+    <style>
+      .nested {
+        background-color: red;
+      }
+      :is(:host, #host6) .nested {
+        background-color: green;
+        width: 30px;
+        height: 30px;
+        margin: 10px;
+      }
+    </style>
+    <div class="nested"></div>
+  `;
+
+  host7.attachShadow({mode: "open"}).innerHTML = `
+    <style>
+      div {
+        background-color: red;
+        width: 30px;
+        height: 30px;
+        margin: 10px;
+      }
+      :is(:host, :host) .nested {
+        background-color: green;
+        width: 30px;
+        height: 30px;
+        margin: 10px;
+      }
+    </style>
+    <div class="nested"></div>
+  `;
+
+  host8.attachShadow({mode: "open"}).innerHTML = `
+    <style>
+      .nested {
+        background-color: green;
+        width: 30px;
+        height: 30px;
+        margin: 10px;
+      }
+      :is(div) .nested {
+        background-color: red;
+        width: 30px;
+        height: 30px;
+        margin: 10px;
+      }
+    </style>
+    <div class="nested"></div>
+  `;
+</script>

Source/WebCore/css/CSSSelector.cpp
@@ -736,7 +736,7 @@ CSSSelector::RareData::RareData(const RareData& other)
         this->selectorList = makeUnique<CSSSelectorList>(*other.selectorList);
 };
 
-auto CSSSelector::RareData::deepCopy() const -> Ref<RareData> 
+auto CSSSelector::RareData::deepCopy() const -> Ref<RareData>
 {
     return adoptRef(*new RareData (*this));
 }
@@ -864,6 +864,11 @@ bool CSSSelector::hasExplicitPseudoClassScope() const
     };
 
     return visitAllSimpleSelectors(check);
+}
+
+bool CSSSelector::isHostPseudoClass() const
+{
+    return match() == Match::PseudoClass && pseudoClass() == PseudoClass::Host;
 }
 
 } // namespace WebCore

Source/WebCore/css/CSSSelector.h
@@ -157,6 +157,7 @@ public:
     bool matchesPseudoElement() const;
     bool isSiblingSelector() const;
     bool isAttributeSelector() const;
+    bool isHostPseudoClass() const;
 
     Relation relation() const { return static_cast<Relation>(m_relation); }
     Match match() const { return static_cast<Match>(m_match); }

Source/WebCore/css/SelectorChecker.h
@@ -111,8 +111,8 @@ public:
     struct LocalContext;
     
 private:
-    MatchResult matchRecursively(CheckingContext&, const LocalContext&, PseudoIdSet&) const;
-    bool checkOne(CheckingContext&, const LocalContext&, MatchType&) const;
+    MatchResult matchRecursively(CheckingContext&, LocalContext&, PseudoIdSet&) const;
+    bool checkOne(CheckingContext&, LocalContext&, MatchType&) const;
     bool matchSelectorList(CheckingContext&, const LocalContext&, const Element&, const CSSSelectorList&) const;
     bool matchHasPseudoClass(CheckingContext&, const Element&, const CSSSelector&) const;
 

Source/WebCore/css/parser/CSSSelectorParser.cpp
@@ -1127,7 +1127,7 @@ void CSSSelectorParser::prependTypeSelectorIfNeeded(const AtomString& namespaceP
     // ::cue), we need a universal selector to set the combinator
     // (relation) on in the cases where there are no simple selectors preceding
     // the pseudo element.
-    bool isHostPseudo = compoundSelector.isHostPseudoSelector();
+    bool isHostPseudo = compoundSelector.isHostPseudoClass();
     if (isHostPseudo && elementName.isNull() && namespacePrefix.isNull())
         return;
     if (tag != anyQName() || isHostPseudo || isShadowDOM)

commit e792146f78cb1b059159befbea138c597a8effa1

Cherry-pick 33507394bab5. rdar://139908140
    [CSS] Don't combine :not() with :host
    https://bugs.webkit.org/show_bug.cgi?id=282960
    rdar://139198548

    Reviewed by Tim Nguyen.

    As a featureless element, only :host matches the shadow host.
    Functional pseudoclasses pass the behavior to their arguments,
    so :is(:host) matches the same as :host (the shadow host).

    This fixes a regression introduced in 281963@main.
    Before 281963, :host combination with a functional pseudoclass
    (or anything but a pseudo-element more generally)
    was early returning "not match".
    281963 has removed the early return for all functional pseudoclass
    (to allow :is() combined with :host, but also :not()).

    Properly supporting :host combining with :not() is tricky
    and will be handled in a followup patch.

    * LayoutTests/TestExpectations:
    * LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-descendant-003-expected.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-descendant-003.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-002-expected.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-002.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-003-expected.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-003.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-004-expected.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-004.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-005-expected.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-005.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-006-expected.html: Added.

Source/WebCore/css/SelectorChecker.cpp
@@ -708,8 +708,12 @@ bool SelectorChecker::checkOne(CheckingContext& checkingContext, LocalContext& c
         // :host doesn't combine with anything except pseudo elements.
         bool isHostPseudoClass = selector.match() == CSSSelector::Match::PseudoClass && selector.pseudoClass() == CSSSelector::PseudoClass::Host;
         bool isPseudoElement = selector.match() == CSSSelector::Match::PseudoElement;
-        // We can early return when we know it's neither :host, a compound like :is(:host), a pseudo-element.
-        if (!isHostPseudoClass && !isPseudoElement && !selector.selectorList())
+        // FIXME: We do not support combining :host with :not() functional pseudoclass. Combination with functional pseudoclass has been allowed for the useful :is(:host) ; but combining with :not() doesn't sound useful like :host():not(:not(:host))
+        // https://bugs.webkit.org/show_bug.cgi?id=283062
+        bool isNotPseudoClass = selector.match() == CSSSelector::Match::PseudoClass && selector.pseudoClass() == CSSSelector::PseudoClass::Not;
+
+        // We can early return when we know it's neither :host, a compound :is(:host) , a pseudo-element.
+        if (!isHostPseudoClass && !isPseudoElement && (!selector.selectorList() || isNotPseudoClass))
             return false;
     }
 

LayoutTests/TestExpectations
@@ -7997,6 +7997,12 @@ webkit.org/b/268995 [ Debug ] fast/misc/out-of-flow-position-inside-mathml-crash
 
 webkit.org/b/261557 media/video-remove-insert-repaints.html [ Pass Crash ]
 
+# CSS match :not(:not(:host))
+# webkit.org/b/283062
+imported/w3c/web-platform-tests/css/css-scoping/host-multiple-002.html [ ImageOnlyFailure ]
+imported/w3c/web-platform-tests/css/css-scoping/host-multiple-004.html [ ImageOnlyFailure ]
+imported/w3c/web-platform-tests/css/css-scoping/host-multiple-006.html [ ImageOnlyFailure ]
+
 # Out-of-flow line break handling
 [ Debug ] fast/text/remove-renderer-and-select-crash.html [ Skip ]
 

LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-descendant-003-expected.html
@@ -1,0 +1,3 @@
+<!DOCTYPE html>
+<p>Test passes if there is a filled green square.</p>
+<div style="width:100px; height:100px; background:green;"></div>

LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-descendant-003.html
@@ -1,0 +1,31 @@
+<!doctype html>
+<meta charset="utf-8">
+<title>:host combined with :not</title>
+<link rel="help" href="https://drafts.csswg.org/selectors/#featureless">
+<link rel="match" href="/css/reference/ref-filled-green-100px-square-only.html">
+<style>
+my-host {
+  display: block;
+  width: 100px;
+  height: 100px;
+  background: red;
+}
+</style>
+<p>Test passes if there is a filled green square.</p>
+<my-host id="host"></my-host>
+<script>
+  host.attachShadow({mode: "open"}).innerHTML = `
+    <style>
+      :host > div {
+        background-color: red;
+        width: 100%;
+        height: 100%;
+      }
+      :host > :not(span) {
+        background-color: green;
+      }
+    </style>
+    <div></div>
+  `;
+
+</script>

LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-002-expected.html
@@ -1,0 +1,3 @@
+<!DOCTYPE html>
+<p>Test passes if there is a filled green square.</p>
+<div style="width:100px; height:100px; background:green;"></div>

LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-002.html
@@ -1,0 +1,23 @@
+<!doctype html>
+<meta charset="utf-8">
+<title>:host</title>
+<link rel="help" href="https://drafts.csswg.org/selectors/#featureless">
+<link rel="match" href="/css/reference/ref-filled-green-100px-square-only.html">
+<p>Test passes if there is a filled green square.</p>
+<div id=host>
+</div>
+<script>
+  host.attachShadow({mode: "open"}).innerHTML = `
+    <style>
+      :host {
+        width: 100px;
+        height: 100px;
+        background-color: red;
+      }
+      :not(:not(:host)):host {
+        background-color: green;
+      }
+    </style>
+  `;
+
+</script>

LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-003-expected.html
@@ -1,0 +1,3 @@
+<!DOCTYPE html>
+<p>Test passes if there is a filled green square.</p>
+<div style="width:100px; height:100px; background:green;"></div>

LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-003.html
@@ -1,0 +1,23 @@
+<!doctype html>
+<meta charset="utf-8">
+<title>:host</title>
+<link rel="help" href="https://drafts.csswg.org/selectors/#featureless">
+<link rel="match" href="/css/reference/ref-filled-green-100px-square-only.html">
+<p>Test passes if there is a filled green square.</p>
+<div id=host>
+</div>
+<script>
+  host.attachShadow({mode: "open"}).innerHTML = `
+    <style>
+      :host {
+        width: 100px;
+        height: 100px;
+        background-color: red;
+      }
+      :is(div, :host) {
+        background-color: green;
+      }
+    </style>
+  `;
+
+</script>

LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-004-expected.html
@@ -1,0 +1,3 @@
+<!DOCTYPE html>
+<p>Test passes if there is a filled green square.</p>
+<div style="width:100px; height:100px; background:green;"></div>

LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-004.html
@@ -1,0 +1,23 @@
+<!doctype html>
+<meta charset="utf-8">
+<title>:host</title>
+<link rel="help" href="https://drafts.csswg.org/selectors/#featureless">
+<link rel="match" href="/css/reference/ref-filled-green-100px-square-only.html">
+<p>Test passes if there is a filled green square.</p>
+<div id=host>
+</div>
+<script>
+  host.attachShadow({mode: "open"}).innerHTML = `
+    <style>
+      :host {
+        width: 100px;
+        height: 100px;
+        background-color: red;
+      }
+      :not(div, :not(:host)) {
+        background-color: green;
+      }
+    </style>
+  `;
+
+</script>

LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-005-expected.html
@@ -1,0 +1,3 @@
+<!DOCTYPE html>
+<p>Test passes if there is a filled green square.</p>
+<div style="width:100px; height:100px; background:green;"></div>

LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-005.html
@@ -1,0 +1,23 @@
+<!doctype html>
+<meta charset="utf-8">
+<title>:host</title>
+<link rel="help" href="https://drafts.csswg.org/selectors/#featureless">
+<link rel="match" href="/css/reference/ref-filled-green-100px-square-only.html">
+<p>Test passes if there is a filled green square.</p>
+<div id=host>
+</div>
+<script>
+  host.attachShadow({mode: "open"}).innerHTML = `
+    <style>
+      :host {
+        width: 100px;
+        height: 100px;
+        background-color: red;
+      }
+      :is(:host):host {
+        background-color: green;
+      }
+    </style>
+  `;
+
+</script>

LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-006-expected.html
@@ -1,0 +1,3 @@
+<!DOCTYPE html>
+<p>Test passes if there is a filled green square.</p>
+<div style="width:100px; height:100px; background:green;"></div>

LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-multiple-006.html
@@ -1,0 +1,23 @@
+<!doctype html>
+<meta charset="utf-8">
+<title>:host</title>
+<link rel="help" href="https://drafts.csswg.org/selectors/#featureless">
+<link rel="match" href="/css/reference/ref-filled-green-100px-square-only.html">
+<p>Test passes if there is a filled green square.</p>
+<div id=host>
+</div>
+<script>
+  host.attachShadow({mode: "open"}).innerHTML = `
+    <style>
+      :host {
+        width: 100px;
+        height: 100px;
+        background-color: red;
+      }
+      :not(:not(:host)):host {
+        background-color: green;
+      }
+    </style>
+  `;
+
+</script>

LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-not-001-expected.html
@@ -1,0 +1,3 @@
+<!DOCTYPE html>
+<p>Test passes if there is a filled green square.</p>
+<div style="width:100px; height:100px; background:green;"></div>

LayoutTests/imported/w3c/web-platform-tests/css/css-scoping/host-not-001.html
@@ -1,0 +1,41 @@
+<!doctype html>
+<meta charset="utf-8">
+<title>:host combined with :not</title>
+<link rel="help" href="https://drafts.csswg.org/selectors/#featureless">
+<link rel="match" href="/css/reference/ref-filled-green-100px-square-only.html">
+<p>Test passes if there is a filled green square.</p>
+<div id=host>
+</div>
+<script>
+  host.attachShadow({mode: "open"}).innerHTML = `
+    <style>
+      :host {
+        width: 100px;
+        height: 100px;
+        background-color: green;
+      }
+      div:host {
+        background-color: red;
+      }
+      :not(div):host {
+        background-color: red;
+      }
+      :host:not(div) {
+        background-color: red;
+      }
+      :host:is(div) {
+        background-color: red;
+      }
+      :is(div):host {
+        background-color: red;
+      }
+      :host:not(:hover) {
+        background-color: red;
+      }
+      :host:not(:defined) {
+        background-color: red;
+      }
+    </style>
+  `;
+
+</script>