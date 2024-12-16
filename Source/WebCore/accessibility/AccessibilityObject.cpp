a32d9df60636f4ee4087eef84170468556aa3542
Tyler Wilcock <tyler_w@apple.com>
2024-12-16 08:44:10 -0800

    Cherry-pick db2c73f6239b. rdar://141174582
        AX: content of <script> elements announced by VoiceOver inside containers with display: contents
        https://bugs.webkit.org/show_bug.cgi?id=283645
        rdar://problem/140493798
    
        Reviewed by Chris Fleizach.
    
        Up until this point, we have got away with not handling display:none (which is the user-agent default `display` for
        script) because it causes the content within to not have a renderer, and thus not be a candidate for AX object creation,
        minus a few exceptions — display:contents, elements within a canvas, a few others, and most notably: elements descending
        from another element involved in a relation.
    
        Because the unrendered text inside the script tag is descendant of an element with a relation, we create an AX object
        for it. And because AccessibilityNodeObject::computeIsIgnored didn't check for a display:none ancestor, we compute
        the unrendered text inside the script to be unignored.
    
        With this commit, we check for display:none in our existing walk up the ancestry as part of the
        AccessibilityObject::defaultObjectInclusion(), and ignore the object if we find one.
    
        * LayoutTests/accessibility/display-none-descendant-of-relation-expected.txt: Added.
        * LayoutTests/accessibility/display-none-descendant-of-relation.html: Added.
        * LayoutTests/platform/glib/TestExpectations: Skip new test.
        * LayoutTests/platform/ios/TestExpectations: Enable new test.
        * LayoutTests/platform/ios/accessibility/display-none-descendant-of-relation-expected.txt: Added.
        * Source/WebCore/accessibility/AXObjectCache.cpp:
        (WebCore::isDOMHidden):
        * Source/WebCore/accessibility/AXObjectCache.h:
        * Source/WebCore/accessibility/AccessibilityNodeObject.cpp:
        (WebCore::AccessibilityNodeObject::computeIsIgnored const):
        * Source/WebCore/accessibility/AccessibilityObject.cpp:
        (WebCore::AccessibilityObject::style const):
        (WebCore::AccessibilityObject::existingStyle const):
        (WebCore::AccessibilityObject::defaultObjectInclusion const):
        * Source/WebCore/accessibility/AccessibilityObject.h:
    
        Canonical link: https://commits.webkit.org/287159@main
    
    Canonical link: https://commits.webkit.org/283286.594@safari-7620-branch

--- Source/WebCore/accessibility/AccessibilityObject.cpp
+++ Source/WebCore/accessibility/AccessibilityObject.cpp
@@ -3060,9 +3060,17 @@ const RenderStyle* AccessibilityObject::style() const
     if (auto* renderer = this->renderer())
         return &renderer->style();
 
-    if (auto* element = this->element())
-        return element->computedStyle();
-    return nullptr;
+    auto* element = this->element();
+    return element ? element->computedStyle() : nullptr;
+}
+
+const RenderStyle* AccessibilityObject::existingStyle() const
+{
+    if (auto* renderer = this->renderer())
+        return &renderer->style();
+
+    auto* element = this->element();
+    return element ? element->existingComputedStyle() : nullptr;
 }
 
 bool AccessibilityObject::isValueAutofillAvailable() const
@@ -4022,9 +4030,9 @@ bool AccessibilityObject::isAXHidden() const
     }) != nullptr;
 }
 
-bool AccessibilityObject::isDOMHidden() const
+bool AccessibilityObject::isRenderHidden() const
 {
-    return WebCore::isDOMHidden(style());
+    return WebCore::isRenderHidden(style());
 }
 
 bool AccessibilityObject::isShowingValidationMessage() const
@@ -4068,6 +4076,10 @@ AccessibilityObjectInclusion AccessibilityObject::defaultObjectInclusion() const
 
     bool ignoreARIAHidden = isFocused();
     if (Accessibility::findAncestor<AccessibilityObject>(*this, false, [&] (const auto& object) {
+        const auto* style = object.existingStyle();
+        if (style && WebCore::isRenderHidden(*style))
+            return true;
+
         return (!ignoreARIAHidden && object.isARIAHidden()) || object.ariaRoleHasPresentationalChildren() || !object.canHaveChildren();
     }))
         return AccessibilityObjectInclusion::IgnoreObject;
