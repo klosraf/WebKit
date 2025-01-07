0d687c61c6ec5cab57fbe50e7931be00e692c3d5
Youenn Fablet <youenn@apple.com>
2025-01-07 12:24:29 -0800

    Cherry-pick 8da7b03ceb9e. rdar://141787428
        rdar://141787428 (J416s/24D34: Screen sharing pause button does not pause)
    
        Reviewed by Eric Carlson.
    
        After https://github.com/WebKit/WebKit/commit/a9b4b55, WebCore is only muting window capture using MediaProducerMutedState::WindowCaptureIsMuted.
        But WKPage SPI was only setting MediaProducerMutedState::ScreenCaptureIsMuted when trying to mute screen share.
        We would then end up muting only screen tracks and not window tracks.
        We update WKPage SPI and alos WKWebView SPI to also handle MediaProducerMutedState::WindowCaptureIsMuted and MediaProducerMutedState::SystemAudioCaptureIsMuted.
    
        Manually tested and covered by updated test.
    
        * Source/WebKit/UIProcess/API/C/WKPage.cpp:
        (WKPageSetMuted):
        * Source/WebKit/UIProcess/API/Cocoa/WKWebView.mm:
        (-[WKWebView _setPageMuted:]):
        * Tools/TestWebKitAPI/Tests/WebKitCocoa/GetDisplayMediaWindowAndScreen.mm:
        (TestWebKitAPI::TEST(WebKit2, GetDisplayMediaWindowAndScreenPrompt)):
    
        Canonical link: https://commits.webkit.org/283286.604@safari-7620-branch
    
    Canonical link: https://commits.webkit.org/283286.608@safari-7620.2.4.11-branch

--- Tools/TestWebKitAPI/Tests/WebKitCocoa/GetDisplayMediaWindowAndScreen.mm
+++ Tools/TestWebKitAPI/Tests/WebKitCocoa/GetDisplayMediaWindowAndScreen.mm
@@ -31,12 +31,17 @@
 #import "Test.h"
 #import "TestWKWebView.h"
 #import "UserMediaCaptureUIDelegate.h"
+#import <WebKit/WKPagePrivate.h>
 #import <WebKit/WKPreferencesPrivate.h>
 #import <WebKit/WKUIDelegatePrivate.h>
 #import <WebKit/WKWebViewConfiguration.h>
 #import <WebKit/WKWebViewConfigurationPrivate.h>
 #import <WebKit/WKWebViewPrivate.h>
 #import <WebKit/WKWebViewPrivateForTesting.h>
+
+@interface WKWebView ()
+- (WKPageRef)_pageForTesting;
+@end
 
 @interface WindowAndScreenCaptureTestView : TestWKWebView
 - (BOOL)haveStream:(BOOL)expected;
@@ -225,7 +230,7 @@ TEST(WebKit2, GetDisplayMediaWindowAndScreenPrompt)
     EXPECT_TRUE([webView _displayCaptureSurfaces] == WKDisplayCaptureSurfaceWindow);
     EXPECT_TRUE([observer displayCaptureSurfaces] == WKDisplayCaptureSurfaceWindow);
 
-    // Mute and unmute
+    // Mute and unmute with ObjC SPI
     completionCalled = false;
     [webView _setDisplayCaptureState:WKDisplayCaptureStateMuted completionHandler:^() {
         completionCalled = true;
@@ -245,6 +250,12 @@ TEST(WebKit2, GetDisplayMediaWindowAndScreenPrompt)
     EXPECT_TRUE([observer displayCaptureState] == WKDisplayCaptureStateActive);
     EXPECT_TRUE([webView _displayCaptureSurfaces] == WKDisplayCaptureSurfaceWindow);
     EXPECT_TRUE([observer displayCaptureSurfaces] == WKDisplayCaptureSurfaceWindow);
+
+    // Mute and unmute with C SPI
+    WKPageSetMuted([webView _pageForTesting], kWKMediaScreenCaptureMuted);
+    EXPECT_TRUE([observer waitForDisplayCaptureState:WKDisplayCaptureStateMuted]);
+    WKPageSetMuted([webView _pageForTesting], kWKMediaScreenCaptureUnmuted);
+    EXPECT_TRUE([observer waitForDisplayCaptureState:WKDisplayCaptureStateActive]);
 
     [webView stringByEvaluatingJavaScript:@"stop()"];
     [webView _setIndexOfGetDisplayMediaDeviceSelectedForTesting:nil];
