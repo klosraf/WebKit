ce1f1a7030879ea402927001434ff5234f436395
Michael Catanzaro <mcatanzaro@redhat.com>
2025-03-19 07:38:08 -0700

    Cherry-pick 292353@main (cda7e1edea14). https://bugs.webkit.org/show_bug.cgi?id=289988
        -Wsubobject-linkage warnings triggered by coroutines
        https://bugs.webkit.org/show_bug.cgi?id=289988
    
        Reviewed by Alex Christensen.
    
        Suppress these warnings for all versions of GCC, since the bug is not
        fixed yet.
    
        * Source/cmake/WebKitCompilerFlags.cmake:
    
        Canonical link: https://commits.webkit.org/292353@main
    
    Canonical link: https://commits.webkit.org/290945.88@webkitglib/2.48

--- Source/cmake/WebKitCompilerFlags.cmake
+++ Source/cmake/WebKitCompilerFlags.cmake
@@ -187,6 +187,11 @@ if (COMPILER_IS_GCC_OR_CLANG)
         WEBKIT_PREPEND_GLOBAL_COMPILER_FLAGS(-Wno-uninitialized)
     endif ()
 
+    # GCC gives false warnings for subobject-linkage <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105595>
+    if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+        WEBKIT_PREPEND_GLOBAL_COMPILER_FLAGS(-Wno-subobject-linkage)
+    endif ()
+
     WEBKIT_PREPEND_GLOBAL_CXX_FLAGS(-Wno-noexcept-type)
 
     # These GCC warnings produce too many false positives to be useful. We'll
